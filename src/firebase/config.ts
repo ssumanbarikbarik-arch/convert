d5577c0e4b1cd3e7cf907b8389225ccfd09b8da3
Suman Barik <ssumanbarikbarik@gmail.com>
2025-12-23 15:11:34 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
 export const firebaseConfig = {
   "projectId": "studio-7586724454-af72c",
   "appId": "1:719723873098:web:40ecb33bef2da9db0b37b3",
-  "storageBucket": "studio-7586724454-af72c.appspot.com",
   "apiKey": "AIzaSyDwVPYnzfKCJvq4GFUg7_-0W7iFkOEgLGQ",
   "authDomain": "studio-7586724454-af72c.firebaseapp.com",
   "measurementId": "",
