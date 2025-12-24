e4e8bd9ab11fc62957d6990bf010c586977ccb8b
Suman Barik <ssumanbarikbarik@gmail.com>
2025-12-24 14:53:47 +0000

    again show integrate the backent storage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:719723873098:web:40ecb33bef2da9db0b37b3",
   "apiKey": "AIzaSyDwVPYnzfKCJvq4GFUg7_-0W7iFkOEgLGQ",
   "authDomain": "studio-7586724454-af72c.firebaseapp.com",
+  "storageBucket": "studio-7586724454-af72c.appspot.com",
   "measurementId": "",
   "messagingSenderId": "719723873098"
 };
